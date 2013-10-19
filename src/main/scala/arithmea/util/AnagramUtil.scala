432458943fcf3a1c2f0626a6710fd4a5a6aab03f
lsteiger4@gmail.com <lsteiger4@gmail.com>
2013-10-19 18:33:42 +0200

    Anagram generation improved.

--- src/main/scala/arithmea/util/AnagramUtil.scala
+++ src/main/scala/arithmea/util/AnagramUtil.scala
@@ -2,6 +2,7 @@ package arithmea.util
 
 import arithmea.Globals
 import java.util.Locale
+import scala.annotation.tailrec
 
 object AnagramUtil {
   val maxWords: Int = 4
@@ -19,43 +20,57 @@ object AnagramUtil {
   }
 
   def generateAnagrams(word: String): List[String] = {
-    def generateAnagrams(): List[List[String]] = {
-      def combinations(key: Key): List[Key] = {
-        def stretchKey(k: Key): Key = {
-          if (!k.isEmpty) {
-            val (hit, other) = k.partition(_.cnt > 1)
-            if (hit.isEmpty) { other }
-            else {
-              Entry(hit.head.chr, 1) :: stretchKey(Entry(hit.head.chr, hit.head.cnt - 1) :: other.tail ::: other)
-            }
-          } else { List.empty }
-        }
-        def compress(keyList: List[Key]): List[Key] = {
-          def compressKey(key: Key): Key = {
-            if (!key.isEmpty) {
-              List(Entry(key.head.chr, key.count(_.chr == key.head.chr))) :::
-                compressKey(key.filterNot(_.chr == key.head.chr)).sorted
-            } else { List.empty }
-          }
-          if (!keyList.isEmpty) { keyList.map(k => compressKey(k.sorted)) } else { List.empty }
-        }
-        val combos = if (!key.isEmpty) {
+    def combinations(key: Key): List[Key] = {
+      key match {
+        case Nil => Nil
+        case _ =>
           val flat = stretchKey(key)
-          compress((1 to flat.size).flatMap(flat.combinations).toList)
-        } else { Nil }
-        (List(Nil) ::: combos).distinct
+          val keyList = (1 to flat.size).flatMap(flat.combinations).toList
+          compressKeyList(keyList).distinct
       }
-      def comb(l: List[String], n: Int): List[List[String]] = {
-        if (n == 0) { List(List.empty) } else l.flatMap(w => comb(l.dropWhile(w != _), n - 1).map(w :: _))
-      }
-      val wordList = List(word.replaceAll("^A-Z", ""))
-      if (!wordList.isEmpty) {
-        val key = getKey(wordList.mkString)
-        val words = combinations(key).flatMap(Globals.anagramMap.get(_).getOrElse(List.empty)).toList
-        val anagrams = (1 to maxWords).flatMap(comb(words, _))
-        anagrams.filter(f => getKey(f.mkString) == key).flatMap(_.permutations).toList
-      } else { List.empty }
     }
-    generateAnagrams.map(_.sorted).distinct.map(_.mkString(" "))
+    def comb(l: List[String], n: Int): List[List[String]] = {
+      n match {
+        case 0 => List(List.empty)
+        case _ => l.flatMap(w => comb(l.dropWhile(w != _), n - 1).map(w :: _))
+      }
+    }
+    val wordList = List(word.replaceAll("^A-Z", ""))
+    wordList match {
+      case Nil => List.empty
+      case _ =>
+        val key = getKey(wordList.mkString)
+        val words = combinations(key).flatMap(Globals.anagramMap.get(_).getOrElse(List.empty))
+        val anagrams = (1 to maxWords).flatMap(comb(words, _))
+        val perms = anagrams.filter(f => getKey(f.mkString) == key).flatMap(_.permutations).toList
+        perms.map(_.sorted).distinct.map(_.mkString(" "))
+    }
+  }
+
+  private def stretchKey(key: Key): Key = {
+    key match {
+      case Nil => List.empty
+      case _ =>
+        val (hit, other) = key.partition(_.cnt > 1)
+        hit match {
+          case Nil => other
+          case _ =>
+            val h = hit.head
+            Entry(h.chr, 1) :: stretchKey(Entry(h.chr, h.cnt - 1) :: other.tail ::: other)
+        }
+    }
+  }
+
+  private def compressKeyList(keyList: List[Key]): List[Key] = {
+    def compressKey(key: Key): Key = {
+      key match {
+        case Nil => List.empty
+        case _ =>
+          val h = key.head
+          val entry = Entry(h.chr, key.count(_.chr == h.chr))
+          List(entry) ::: compressKey(key.filterNot(_.chr == h.chr))
+      }
+    }
+    keyList.map(compressKey(_).sorted)
   }
 }
